d000134cf19ebd34330b3e7eb0fc2bbfe1c9f091
Shiki <shikishiji@gmail.com>
2014-02-07 13:58:20 +0800

    Reformatted MECustomBadge

--- MESegmentedControl/MECustomBadge.h
+++ MESegmentedControl/MECustomBadge.h
@@ -30,30 +30,37 @@
 #import <QuartzCore/QuartzCore.h>
 
 @interface MECustomBadge : UIView {
-
-	NSString *badgeText;
-	UIColor *badgeTextColor;
-	UIColor *badgeInsetColor;
-	UIColor *badgeFrameColor;
-	BOOL badgeFrame;
-	BOOL badgeShining;
-	CGFloat badgeCornerRoundness;
-	CGFloat badgeScaleFactor;
+  NSString *badgeText;
+  UIColor *badgeTextColor;
+  UIColor *badgeInsetColor;
+  UIColor *badgeFrameColor;
+  BOOL badgeFrame;
+  BOOL badgeShining;
+  CGFloat badgeCornerRoundness;
+  CGFloat badgeScaleFactor;
 }
 
-@property(nonatomic,retain) NSString *badgeText;
-@property(nonatomic,retain) UIColor *badgeTextColor;
-@property(nonatomic,retain) UIColor *badgeInsetColor;
-@property(nonatomic,retain) UIColor *badgeFrameColor;
+@property (nonatomic, retain) NSString *badgeText;
+@property (nonatomic, retain) UIColor *badgeTextColor;
+@property (nonatomic, retain) UIColor *badgeInsetColor;
+@property (nonatomic, retain) UIColor *badgeFrameColor;
 
-@property(nonatomic,readwrite) BOOL badgeFrame;
-@property(nonatomic,readwrite) BOOL badgeShining;
+@property (nonatomic, readwrite) BOOL badgeFrame;
+@property (nonatomic, readwrite) BOOL badgeShining;
 
-@property(nonatomic,readwrite) CGFloat badgeCornerRoundness;
-@property(nonatomic,readwrite) CGFloat badgeScaleFactor;
+@property (nonatomic, readwrite) CGFloat badgeCornerRoundness;
+@property (nonatomic, readwrite) CGFloat badgeScaleFactor;
 
-+ (MECustomBadge *) customBadgeWithString:(NSString *)badgeString;
-+ (MECustomBadge *) customBadgeWithString:(NSString *)badgeString withStringColor:(UIColor*)stringColor withInsetColor:(UIColor*)insetColor withBadgeFrame:(BOOL)badgeFrameYesNo withBadgeFrameColor:(UIColor*)frameColor withScale:(CGFloat)scale withShining:(BOOL)shining;
-- (void) autoBadgeSizeWithString:(NSString *)badgeString;
++ (MECustomBadge *)customBadgeWithString:(NSString *)badgeString;
+
++ (MECustomBadge *)customBadgeWithString:(NSString *)badgeString
+                         withStringColor:(UIColor *)stringColor
+                          withInsetColor:(UIColor *)insetColor
+                          withBadgeFrame:(BOOL)badgeFrameYesNo
+                     withBadgeFrameColor:(UIColor *)frameColor
+                               withScale:(CGFloat)scale
+                             withShining:(BOOL)shining;
+
+- (void)autoBadgeSizeWithString:(NSString *)badgeString;
 
 @end
